3b9521f3c01fdf58d73a05b17731acf2232d952a
Anuj Vashishth <vashishthaanuj07@gmail.com>
2026-01-12 16:54:23 +0530

    second question

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 //Reverse Words in a Sentence
 //Input: "Java is fun" → Output: "fun is Java"
 
-let string = "Java is fun";
+// let string = "Java is fun";
 
-console.log(string.split(" ").reverse().join(" "));
+// console.log(string.split(" ").reverse().join(" "));
+
+
+
+function isRotation(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  return (str1 + str1).includes(str2);
+}
+
+// Test
+console.log(isRotation("ABCD", "CDAB")); 
+console.log(isRotation("ABCD", "ACBD")); 
+
 
 
 
