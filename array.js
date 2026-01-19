2ba35dd286c7c92cc4f2838bc482f4c33868c598
Anuj Vashishth <vashishthaanuj07@gmail.com>
2026-01-19 11:03:23 +0530

    four commit

---
+++ array.js
@@ -0,0 +1,13 @@
+let arr = [10, 30, 40];
+arr.push(60);
+console.log(arr);
+
+
+arr.pop();
+console.log(arr);
+
+arr.shift()
+console.log(arr);
+
+arr.unshift(70);
+console.log(arr);
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 //Reverse Words in a Sentence
 //Input: "Java is fun" → Output: "fun is Java"
 
-// let string = "Java is fun";
-
-// console.log(string.split(" ").reverse().join(" "));
+ let string = "Java is fun";
+ console.log(string.split(" ").reverse().join(" "));
 
 
 
@@ -16,6 +15,14 @@ function isRotation(str1, str2) {
 // Test
 console.log(isRotation("ABCD", "CDAB")); 
 console.log(isRotation("ABCD", "ACBD")); 
+
+let s1 = "Hello";
+let s2 = 'World';
+let s3 = `Node.js Backend`;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
 
 
 
