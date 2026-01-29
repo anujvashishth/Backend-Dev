fce05baa56a5e86454127cf1b21a8791d223aef8
Anuj Vashishth <vashishthaanuj07@gmail.com>
2026-01-29 15:33:10 +0530

    Add Library System

---
+++ LibaryManagementSystem/index.js
@@ -0,0 +1,8 @@
+// import book from "./src/book.js";
+// book( "Math", "N.P Mishra", 500)
+
+// import member from "./src/member.js";
+// member ("Anuj", "gold")
+
+import BorrowRecord from "./src/BorrowRecord.js";
+BorrowRecord()
--- Todo/src/createTodo.js
+++ Todo/src/createTodo.js
@@ -8,7 +8,16 @@ function createTodo(name, task){
         }
         if(fs.existsSync("todo.json")){
             let data = JSON.parse(fs.readFileSync("todo.json", "utf-8"))
-            
+            for(let i=0; i<data.length && data.length>0; i++){
+                if(data[i].name===name && data.length>0){
+                    data[i].todo.push(obj)
+                }
+            }
+            fs.writeFileSync("todo.json", JSON.stringify(data, null, 2))
         }
     }
-}
+    catch(error){
+        console.log(error)
+    }
+}export default createTodo
+       
