c992f2b4c39df43326a57c5b318dac8e265d3f86
Anuj Vashishth <vashishthaanuj07@gmail.com>
2026-04-13 17:58:26 +0530

    [Anuj].add project structred

--- middle/order.middle.js
+++
@@ -1,5 +0,0 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { StatusCodes } from "http-status-pro-js";
-
-
--- routes/user.router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import {userlogin} from "../controller/user.controller.js"
-
-let router = express.Router();
-router.post("/login", userlogin)
-
-export default router;
